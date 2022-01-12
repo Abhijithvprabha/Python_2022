be748a4c488c1723086c1c49af68c29d4ae5e1fe
Abhijith Vilangapparayil Prabha <97640430+Abhijithvprabha@users.noreply.github.com>
2022-01-12 18:23:25 -0500

    Add files via upload

---
+++ length_of_a_string.py
@@ -0,0 +1,17 @@
+#Write your code below this line 👇
+#This program finds the length of a string that the user enters
+#The len(o) function will find the length of the string where o is the string here!
+
+name=input("What's your name?")
+l= len(name)
+print(l)
+
+
+
+
+
+
+
+
+
+
