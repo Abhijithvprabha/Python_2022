5bd060dc3bb850fe4090f1e686cd8b2ca86642ab
Abhijith Vilangapparayil Prabha <97640430+Abhijithvprabha@users.noreply.github.com>
2022-01-15 15:28:27 -0500

    Add files via upload

---
+++ age_to_ninty_years.py
@@ -0,0 +1,23 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+age = int(age)
+#find the left years to the age 90
+untill_ninty = 90 - age
+days = untill_ninty * 365
+weeks = untill_ninty * 52
+months = untill_ninty * 12
+
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
+
+
+
+
+
+
+
+
+
+
