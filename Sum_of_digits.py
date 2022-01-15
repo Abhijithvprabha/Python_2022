659ab7653467935db02035ade9fe75e3e1fe1559
Abhijith Vilangapparayil Prabha <97640430+Abhijithvprabha@users.noreply.github.com>
2022-01-15 12:59:25 -0500

    Add files via upload

---
+++ Sum_of_digits.py
@@ -0,0 +1,20 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+first_letter = two_digit_number[0]
+second_letter =two_digit_number[1]
+# print(first_letter)
+# print(type(first_letter))
+first_letter = int(first_letter)
+second_letter = int(second_letter)
+
+sum_of_digits = first_letter + second_letter
+
+print(sum_of_digits)
+
+
+
