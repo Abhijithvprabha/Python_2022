3d75fe89df1e5d0848b07e324d5b1791f0f2db1a
Abhijith Vilangapparayil Prabha <97640430+Abhijithvprabha@users.noreply.github.com>
2022-01-16 15:02:27 -0500

    Add files via upload

---
+++ vote_Eligibility.py
@@ -0,0 +1,12 @@
+# write a program that take the age from the user, check vote eligibility !
+
+print("welcome to test your voting eligibility")
+
+age = int(input("Please enter your age "))
+
+if age > 18 :
+    print("you can vote")
+else:
+    print(":No vote")
+
+
