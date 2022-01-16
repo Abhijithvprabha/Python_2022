4dccf452fb1c27956c0c60cc70a45385fcf191db
Abhijith Vilangapparayil Prabha <97640430+Abhijithvprabha@users.noreply.github.com>
2022-01-16 15:18:08 -0500

    Add files via upload

---
+++ 7_divisible_or_not.py
@@ -0,0 +1,10 @@
+#Write a program to check whether a number is divisible by 7 or not.
+
+number = int((input("please enter the number you want to test? ")))
+if number % 7 == 0:
+    print("yes, divisible by 7")
+else:
+    print("not divisible")
+
+
+
