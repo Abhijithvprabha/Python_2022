af421a021e5785d7e4adb0c3d3e4ee89659bb341
Abhijith Vilangapparayil Prabha <97640430+Abhijithvprabha@users.noreply.github.com>
2022-01-16 15:24:31 -0500

    Add files via upload

---
+++ hell0_5_divisible.py
@@ -0,0 +1,10 @@
+#Write a program to check whether a number is divisible by 5 or not.
+
+number = int((input("enter the number to check ")))
+if number % 5 == 0:
+    print("hello")
+else:
+    print("bye")
+
+
+
