4f94cf3a437e714768f01487b6c73433de65f134
Abhijith Vilangapparayil Prabha <97640430+Abhijithvprabha@users.noreply.github.com>
2022-03-26 14:33:01 -0400

    OOPS_DAY17
    Object-oriented programming beginning

---
+++ day17.py
@@ -0,0 +1,5 @@
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon",["pikachu", "squrite", "charmander", "mangoe"])
+table.add_column("type",["fire","eater","electric","wind"])
+print(table)
