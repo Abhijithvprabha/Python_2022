ec04d91c2904946f0df4dca80ed331f38696de3a
Abhijith Vilangapparayil Prabha <97640430+Abhijithvprabha@users.noreply.github.com>
2022-03-31 23:32:50 -0400

    using panda library
    using panda library   to work on CSV files

--- main.py
+++ main.py
@@ -2,12 +2,17 @@
 #     data = weather_file.readlines()
 #     print(data)
 
-import csv
-with open("weather_data.csv") as weather_file:
-    data = csv.reader(weather_file)
-    temprature = []
-    for row in data:
-        if row[1] != 'temp':
-            temprature.append(int(row[1]))
-    print(temprature)
+# import csv
+# with open("weather_data.csv") as weather_file:
+#     data = csv.reader(weather_file)
+#     temprature = []
+#     for row in data:
+#         if row[1] != 'temp':
+#             temprature.append(int(row[1]))
+#     print(temprature)
 
+import pandas as pd
+
+data = pd.read_csv("weather_data.csv")
+print(data)
+print(data['temp'])
