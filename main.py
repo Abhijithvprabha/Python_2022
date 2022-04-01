0b8884592965c6f006cc89261870bb5a1225736f
Abhijith Vilangapparayil Prabha <97640430+Abhijithvprabha@users.noreply.github.com>
2022-03-31 23:24:26 -0400

    CSV data , import csv
    working on csv data, how to take the values out and create list , extract data

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# TODO: Create a letter using starting_letter.txt
-PLACEHOLDER = '[name]'
+# with open("weather_data.csv") as weather_file:
+#     data = weather_file.readlines()
+#     print(data)
 
-with open("./Input/Names/invited_names.txt", mode='r') as name_file:
-    names = name_file.readlines()
-    # print(names)
+import csv
+with open("weather_data.csv") as weather_file:
+    data = csv.reader(weather_file)
+    temprature = []
+    for row in data:
+        if row[1] != 'temp':
+            temprature.append(int(row[1]))
+    print(temprature)
 
-with open("./Input/Letters/starting_letter.txt") as letter_file:
-    contents = letter_file.read()
-
-for name in names:
-    striped_name = name.strip()
-    new_letter = contents.replace(PLACEHOLDER, striped_name)
-    # print(new_letter)
-    with open(f"./Output/ReadyToSend/letter_for_{striped_name}.txt", mode='w') as completed_letter:
-        completed_letter.write(new_letter)
